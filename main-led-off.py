c1c32f6fa7c9ef3ebdf3e96925664d28ab5352d8
Mikhail Mineev <mikevbif@yandex.ru>
2018-12-11 03:38:07 +0300

    Change logic

--- main-led-off.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-import RPi.GPIO as GPIO
-gpio_pin_number=16
-GPIO.setmode(GPIO.BCM) # BCM pin numbering
-GPIO.setwarnings(False)
-GPIO.setup(gpio_pin_number, GPIO.OUT)
-GPIO.output(gpio_pin_number, GPIO.LOW)
--- main-led-toggle.py
+++ main-led-toggle.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python
 import RPi.GPIO as GPIO
-gpio_pin_number=18
+gpio_pin_number=16
 GPIO.setmode(GPIO.BCM) # BCM pin numbering
 GPIO.setwarnings(False)
 GPIO.setup(gpio_pin_number, GPIO.OUT)
+GPIO.output(gpio_pin_number, GPIO.HIGH)
+
+time.sleep(0.5)
+
 GPIO.output(gpio_pin_number, GPIO.LOW)
+

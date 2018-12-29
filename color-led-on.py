36d513dfd989ede9d8edb6e99eb25656977ab365
Mikhail Mineev <mikevbif@yandex.ru>
2018-12-29 22:48:38 +0300

    fix toggle not working properly

--- color-led-on.py
+++ color-led-on.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python
 import RPi.GPIO as GPIO
+import time
 
+gpio_pin_number=16
 read_pin_number=17
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 GPIO.setup(read_pin_number, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 if GPIO.input(read_pin_number):
-	print('1')
+	GPIO.setup(gpio_pin_number, GPIO.OUT)
+	GPIO.output(gpio_pin_number, GPIO.HIGH)
+	time.sleep(0.1)
+	GPIO.output(gpio_pin_number, GPIO.LOW)
+	time.sleep(0.1)
---
+++ main-led-off.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+import RPi.GPIO as GPIO
+import time
+
+gpio_pin_number=18
+read_pin_number=19
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(read_pin_number, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+if GPIO.input(read_pin_number):
+	GPIO.setup(gpio_pin_number, GPIO.OUT)
+	GPIO.output(gpio_pin_number, GPIO.HIGH)
+	time.sleep(0.1)
+	GPIO.output(gpio_pin_number, GPIO.LOW)
+	time.sleep(0.1)
